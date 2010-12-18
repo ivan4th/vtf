ebf9aeb7733b90bcde583edf54f6bd664e39369a
Ivan Shvedunov <ivan4th@gmail.com>
2010-12-18 03:42:47 +0300

    Introduced fixtures, EXPECTING, DEFTEST and other useful things.

--- package.lisp
+++ package.lisp
@@ -1,4 +1,10 @@
 (defpackage :vtf
-    (:export #:is #:is-true #:is-false #:fail #:signals #:test #:def-suite #:in-suite
-             #:run-tests #:run-all-tests)
+    (:export #:is #:is-true #:is-false #:fail #:signals
+             #:*fixture* #:*last-fixture*
+             #:test #:deftest
+             #:define-fixture #:with-fixture #:setup-fixture
+             #:def-suite #:in-suite
+             #:setup #:teardown
+             #:run-tests #:run-all-tests
+             #:logged-fixture #:expecting #:<< #:<<< #:==>)
   (:use :cl :alexandria))
--- vtf.lisp
+++ vtf.lisp
@@ -1,5 +1,7 @@
 (in-package :vtf)
 
+;;; ASSERTIONS
+
 (define-condition test-failure (serious-condition) 
   ((message :initform nil :initarg :message :accessor test-failure-message))
   (:report (lambda (condition stream)
@@ -53,12 +55,12 @@
                   ,(if neg-p
                        `(when (,pred ,exp ,act)
                           ,(expand-fail fmt args
-                                        "~s evaluated to ~s which is ~s to ~s = ~s while it shouldn't"
+                                        "~s evaluated to~%~s which is ~s to ~s =~%~s while it shouldn't"
                                         `(quote ,actual) act `(quote ,pred)
                                         `(quote ,expected) exp))
                        `(unless (,pred ,exp ,act)
                           ,(expand-fail fmt args
-                                        "~s evaluated to ~s which isn't ~s to ~s = ~s"
+                                        "~s evaluated to~%~s which isn't ~s to ~s =~%~s"
                                         `(quote ,actual) act `(quote ,pred)
                                         `(quote ,expected) exp)))))))
           (t
@@ -84,13 +86,168 @@
                        `(quote ,expr) v)))))
 
 (defmacro signals (spec &body body)
-  (destructuring-bind (condition fmt &rest args) (ensure-list spec)
+  (destructuring-bind (condition &optional fmt &rest args) (ensure-list spec)
     `(handler-case (progn
                      (block nil ,@body)
                      ,(expand-fail fmt args
                                    "expected condition ~s not signalled"
                                    `(quote ,condition)))
        (,condition () nil))))
+
+;;; PROTOCOL
+
+(defgeneric test-items (object))
+
+(defgeneric test-item-name (object)
+  (:method ((object t)) nil))
+
+(defgeneric run-tests (object)
+  (:method (object)
+    (let ((n-pass 0)
+          (failed '()))
+      (dolist (item (test-items object))
+        (setup item)
+        (unwind-protect
+             (multiple-value-bind (np f)
+                 (run-tests item)
+               (incf n-pass np)
+               (appendf failed f))
+          (teardown item)))
+      (format t "~&---~%*** TOTAL~@[ IN ~A~]: ~s tests, ~s passed, ~s failed~@[: ~s~]~%"
+              (test-item-name object)
+              (+ n-pass (length failed))
+              n-pass (length failed) failed)
+      (values n-pass failed))))
+
+(defgeneric setup (fixture)
+  (:method ((fixture t)) nil))
+
+(defgeneric teardown (fixture)
+  (:method ((fixture t)) nil))
+
+;;; TEST CASE
+
+(defclass test-case ()
+  ((name :accessor name :initarg :name)
+   (test-function :accessor test-function :initarg :test-function)))
+
+(defmethod run-tests ((object test-case))
+  (handler-case
+      (progn
+        (funcall (test-function object))
+        (format t "~&*** PASS: ~s~%" (name object))
+        (values 1 '()))
+    (serious-condition (c)
+      (format t "~&*** FAIL: ~s~%~s: ~a~%---~%" (name object) (type-of c) c)
+      (values 0 (list (name object))))))
+
+;;; FIXTURES
+
+(defvar *package-fixtures* (make-hash-table))
+(defvar *fixture-test-cases* (make-hash-table))
+(defvar *fixture* nil)
+(defvar *last-fixture* nil)
+
+(defmethod test-item-name ((fixture standard-object))
+  (format nil "FIXTURE ~s" (type-of fixture)))
+
+(defmethod test-items ((fixture standard-object))
+  (reverse (gethash (type-of fixture) *fixture-test-cases*)))
+
+(defmethod run-tests ((fixture standard-object))
+  (let ((*fixture* (setf *last-fixture* fixture)))
+    (call-next-method)))
+
+(defmacro define-fixture (name direct-superclasses direct-slots &rest options)
+  `(progn
+     (defclass ,name ,direct-superclasses ,direct-slots ,@options)
+     (setf (get ',name 'fixture-p) t)
+     (pushnew ',name (gethash (symbol-package ',name) *package-fixtures*))))
+
+;;; PACKAGES
+
+(defvar *package-test-cases* (make-hash-table))
+
+(defmethod test-item-name ((package package))
+  (format nil "PACKAGE ~s" (package-name package)))
+
+(defmethod test-items ((package package))
+  (append (mapcar #'make-instance
+                  (reverse (gethash (find-package package) *package-fixtures*)))
+          (reverse (gethash (find-package package) *package-test-cases*))))
+
+;;; CONVENIENCE
+
+(defmethod test-items ((object (eql t)))
+  (sort (union (hash-table-keys *package-test-cases*)
+               (hash-table-keys *package-fixtures*))
+        #'string< :key #'package-name))
+
+(defmethod run-tests ((name symbol))
+  (cond ((eq t name)
+         (call-next-method))
+        ((get name 'test-case)
+         (run-tests (get name 'test-case)))
+        ((get name 'fixture-p)
+         (run-tests (make-instance name)))
+        ((find-package name)
+         (run-tests (find-package name)))
+        (t
+         (error "cannot locate any tests for the specified name: ~s" name))))
+
+(defun run-all-tests ()
+  (run-tests t))
+
+;;; TEST and DEFTEST macros
+
+(defmacro test (spec &body body)
+  #+nil (simple-style-warning "TEST is retained only for 5am compatibility")
+  (destructuring-bind (name &key suite)
+      (ensure-list spec)
+    (declare (ignore suite)) ;; FIXME: 5am compat
+    `(deftest ,name () () ,@body)))
+
+(defun setup-fixture (name)
+  (let ((fixture (make-instance name)))
+    (setup fixture)
+    fixture))
+
+(defmacro with-fixture (slots expr &body body)
+  `(let ((*fixture* ,expr)) ;; needed for direct invocation
+     ,@(if slots
+           `((with-slots ,slots *fixture*
+               ,@body))
+           body)))
+
+(defmacro deftest (name slots (&optional fixture-spec &rest options) &body body)
+  (declare (ignore options))
+  (assert (or (not (null fixture-spec))
+              (null slots))
+          () "cannot specify slots without fixture")
+  (assert (or (null fixture-spec)
+              (typep fixture-spec '(or symbol
+                                    (cons symbol (cons symbol nil)))))
+          () "invalid fixture spec ~s" fixture-spec)
+  (multiple-value-bind (fixture-var fixture-name)
+      (etypecase fixture-spec
+        (null (values (gensym) nil))
+        (symbol (values (gensym) fixture-spec))
+        (t (values (first fixture-spec) (second fixture-spec))))
+    `(progn
+       (defun ,name (&optional ,(if fixture-name
+                                    `(,fixture-var (setup-fixture ',fixture-name))
+                                    fixture-var))
+         (with-fixture ,slots ,fixture-var ,@body))
+       (pushnew ',name
+                ,(if fixture-name
+                     `(gethash ',fixture-name *fixture-test-cases*)
+                     `(gethash (symbol-package ',name) *package-test-cases*)))
+       (setf (get ',name 'test-case)
+             (make-instance 'test-case
+                            :name ',name
+                            :test-function ',name)))))
+
+;;; 5AM COMPAT (to be removed)
 
 (defmacro in-suite (name)
   (declare (ignore name))
@@ -102,44 +259,33 @@
   #+nil (simple-style-warning "DEF-SUITE is retained only for 5am compatibility")
   nil)
 
-(defvar *package-test-cases* (make-hash-table))
+;;; LOGGED-FIXTURE & related
 
-(defmacro test (spec &body body)
-  #+nil (simple-style-warning "TEST is retained only for 5am compatibility")
-  (let ((name (ensure-car spec)))
+(defclass logged-fixture ()
+  ((log :accessor log-of :initform '())))
+
+(defmacro expecting (&body body)
+  (let* ((p (or (position '==> body)
+                (error "==> not found")))
+         (actual-body (subseq body 0 p))
+         (expected (subseq body (1+ p))))
     `(progn
-       (defun ,name ()
-         ,@body)
-       (pushnew ',name (gethash *package* *package-test-cases*)))))
+       (let ((log-start (log-of *fixture*)))
+         (progn ,@actual-body)
+         (let ((log-tail (reverse (ldiff (log-of *fixture*) log-start))))
+           (is (equal (list ,@(loop for item in expected
+                                    collect (if (atom item)
+                                                item
+                                                `(list ,@item))))
+                      log-tail)))))))
 
-(defun run-tests (&optional (package *package*))
-  (let ((n-pass 0)
-        (failed '()))
-    (dolist (test (reverse (gethash (find-package package) *package-test-cases*)))
-      (handler-case
-          (progn
-            (funcall test)
-            (incf n-pass)
-            (format t "~&PASS: ~s~%" test))
-        (serious-condition (c)
-          (push test failed)
-          (format t "~&FAIL: ~s~%~s: ~a~%---~%" test (type-of c) c))))
-    (format t "~&---~%TOTAL IN PACKAGE ~s: ~s tests, ~s passed, ~s failed~@[: ~s~]~%"
-            (package-name package) (+ n-pass (length failed))
-            n-pass (length failed) failed)
-    (values n-pass failed)))
+(defun << (&rest things)
+  (dolist (item things)
+    (push item (log-of *fixture*))))
 
-(defun run-all-tests ()
-  (let ((n-pass 0)
-        (failed '()))
-    (dolist (package (sort (mapcar #'package-name
-                                   (hash-table-keys *package-test-cases*))
-                           #'string<))
-      (multiple-value-bind (np f)
-          (run-tests package)
-        (incf n-pass np)
-        (appendf failed f)))
-    (format t "~&---~%TOTAL: ~s tests, ~s passed, ~s failed~@[: ~s~]~%"
-            (+ n-pass (length failed))
-            n-pass (length failed) failed)
-    (values n-pass failed)))
+(defmacro <<< (&rest things)
+  (flet ((expand (thing)
+           (if (or (stringp thing) (keywordp thing))
+               thing
+               `(list ',thing := ,thing))))
+    `(<< ,@(mapcar #'expand things))))
